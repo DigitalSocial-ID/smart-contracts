7ce88cbde9a7a5ded2a8b74c017123968996361f
mnm458 <mohaknainmalhotra@gmail.com>
2023-01-22 14:07:21 +1100

    DSID fin SAT base contract

---
+++ contracts/FinSat.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./OCMeta.sol";
+
+// Author: @mnm458
+contract FinSat is ERC721, ERC721URIStorage, Ownable, OCMeta {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor(string memory name, string memory symbol) ERC721("DSID Finance SAT", "DFS") {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    //Every time this code will run, the require statement will check: 
+
+    function _beforeTokenTransfer(
+        address from, 
+        address to, 
+        uint256 tokenId,
+        uint256 batchSize
+        ) internal override virtual {
+        require(from == address(0), "Err: token transfer is BLOCKED");   
+        super._beforeTokenTransfer(from, to, tokenId,batchSize);  
+        }
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        require(_exists(tokenId), "tokenId doesn't exist");
+        return _createTokenURI(tokenId);
+    }
+
+    function contractURI() public view virtual returns (string memory) { 
+        return _createContractURI();
+    }
+}
